36b03cddaa056eefb4282ff04a0cfb1b1874800d
tubaaNur <yesiltastubanur@gmail.com>
2023-08-15 22:41:36 +0300

    Initial Commit

---
+++ yemekSepetiCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yemekSepetiCloneApp
+//
+//  Created by Tuba Nur  on 15.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
